e0fd890ab9659473a2410da2d16fa0e171f8aadc
JoseEscobar128 <guillermoescobar128@gmail.com>
2023-07-13 18:06:58 -0600

    tabla de estadio

--- env.ts
+++ env.ts
@@ -1,6 +1,7 @@
 export const environment = {
     apiUrl : 'http://127.0.0.1:8000/api/futbolista',
     apiUrlDiv: 'http://127.0.0.1:8000/api/division',
+    apiUrlEstad:'http://127.0.0.1:8000/api/estadio',
     apiUrlSP : 'http://127.0.0.1:8000/api',
     webUrl : 'http://localhost:4200/',
     errorUrl : 'http://localhost:4200/404',
--- src/app/services/login/estadios/estadio.service.ts
+++ src/app/services/login/estadios/estadio.service.ts
@@ -11,39 +11,39 @@ type NewType = Estadio;
 })
 export class EstadioService {
 
-  private apiUrl = environment.apiUrl;
+  private apiUrlEstad = environment.apiUrlEstad;
   constructor(private http:HttpClient) {  }
 
   getEstadio(id:number): Observable<Estadio> {
     const token = localStorage.getItem('token');
     const headers = new HttpHeaders({ "Accept": "application/json", "Authorization": `Bearer ${token}` });
-    const url = `${this.apiUrl}/${id}`;
+    const url = `${this.apiUrlEstad}/${id}`;
     return this.http.get<Estadio>(url, { headers });
   }
 
   getEstadios(): Observable<NewType[]> {
-    return this.http.get<Estadio[]>(`${this.apiUrl}/`);
+    return this.http.get<Estadio[]>(`${this.apiUrlEstad}/`);
   }
 
   createEstadio(estadio: Estadio): Observable<Estadio>
   {
     const token = localStorage.getItem('token');
     const headers = new HttpHeaders({ "Accept": "application/json", "Authorization": `Bearer ${token}` });
-    const url = `${this.apiUrl}/`;
+    const url = `${this.apiUrlEstad}/`;
     return this.http.post<Estadio>(url, estadio, { headers });
   }
   updateEstadio(id: number, estadio: Estadio): Observable<Estadio>
   {
     const token = localStorage.getItem('token');
     const headers = new HttpHeaders({ "Accept": "application/json", "Authorization": `Bearer ${token}` });
-    const url = `${this.apiUrl}/${id}`;
+    const url = `${this.apiUrlEstad}/${id}`;
     return this.http.put<Estadio>(url, estadio, { headers });
   }
 
   deleteEstadio(id: number): Observable<void>{
   const token = localStorage.getItem('token');
   const headers = new HttpHeaders({ "Accept": "application/json", "Authorization": `Bearer ${token}` });
-  const url = `${this.apiUrl}/${id}`;
+  const url = `${this.apiUrlEstad}/${id}`;
   return this.http.delete<void>(url, { headers });
   }
 }
