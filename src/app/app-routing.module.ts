9f3a9e7e3673c77a0e1ca648df466fa42788fac4
JoseEscobar128 <guillermoescobar128@gmail.com>
2023-07-13 18:22:48 -0600

    modificaciones en la tabla de jugadores, dashboard y create. ya esta ordenado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { DivisionComponent } from './components/secure/division/division.component';
 import { EstadioComponent } from './components/secure/estadio/estadio.component';
+import { FutbolistasComponent } from './components/secure/futbolistas/futbolistas.component';
+import { CreateComponent } from './components/secure/futbolistas/Futbolista/create/create.component';
 
 
 
@@ -18,7 +20,12 @@ const routes: Routes = [
 //  { path: 'dashboard', component: DashboardComponent, canActivate: [tokenGuard]},
 { path: 'dashboard', component: DashboardComponent},
   {path: 'division', component: DivisionComponent},
-  {path: 'estadio', component: EstadioComponent}
+  {path: 'estadio', component: EstadioComponent},
+  {path:'futbolistas', component: FutbolistasComponent},
+
+
+  //Botones
+  {path: 'crearFutbolista', component:CreateComponent}
 ];
 
 @NgModule({
